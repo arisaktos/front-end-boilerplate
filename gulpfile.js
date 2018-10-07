336eb31b8224182b2887ef9c25e85d25f9f3e52a
Alicja <arisusaktos@gmail.com>
2018-10-07 22:06:12 +0200

    changed runsequence

--- gulpfile.js
+++ gulpfile.js
@@ -63,7 +63,7 @@ gulp.task('default', function(callback) {
  
 gulp.task('build', function (callback) {
   runSequence('clean:dist', 
-    ['sass', 'useref', 'images'],
+    'sass', 'useref', 'images',
     callback
   )
 });
